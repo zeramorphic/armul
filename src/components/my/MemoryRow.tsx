4d80d52c8b45acd8a660c6b009eb10d47bb1a471
Sky Wilshaw <sky.wilshaw@gmail.com>
2026-01-10 14:12:37 +0000

    Bundle fonts

--- src/components/my/MemoryRow.tsx
+++ src/components/my/MemoryRow.tsx
@@ -170,7 +170,7 @@ export default function MemoryRow(props: RowComponentProps<MemoryRowProps>) {
     <div className="text-(--muted-foreground) flex-none w-[80px]">{renderAddress(addr, "text-(--extremely-muted-foreground)")}</div>
     <div className={cn(props.mode === "Disassemble" ? "text-(--muted-foreground)" : "", "flex-none w-[80px]")}>{renderAddress(info?.value ?? 0, "text-(--extremely-muted-foreground)")}</div>
     <div className="flex-none w-[180px]">{body}</div>
-    {props.mode === "Disassemble" ? <div className="flex-1 text-emerald-600">{info?.comment}</div> : <></>}
+    {props.mode === "Disassemble" ? <div className="flex-1 text-emerald-600 italic">{info?.comment}</div> : <></>}
   </div>;
 }
 
